2b1e4ddd074cb1c996bb12135e1a00cc7b9aec6d
Matt Boland <mattjamesboland@gmail.com>
2020-07-28 18:16:54 +0000

    added app.config and connection to mongodb

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'online-cookbook-milestonethree'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-olf6t.mongodb.net/online-cookbook-milestonethree?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/get_recipes')
 def hello():
     return 'Hello World'
 
