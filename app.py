15c8a3396fe177b5d6153211572cae1be237ab3b
Matt Boland <mattjamesboland@gmail.com>
2020-07-28 18:23:43 +0000

    fixed error in mongodb name and URI

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'online-cookbook-milestonethree'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-olf6t.mongodb.net/online-cookbook-milestonethree?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'online_cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-olf6t.mongodb.net/online_cookbook?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
