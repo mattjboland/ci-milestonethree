037e1d6587b8e94187f827115000e7bd2c3c8778
Matt Boland <mattjamesboland@gmail.com>
2020-07-28 18:20:58 +0000

    added render_template, redirect, request and url_for to import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -9,6 +9,7 @@ app.config["MONGO_DBNAME"] = 'online-cookbook-milestonethree'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-olf6t.mongodb.net/online-cookbook-milestonethree?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_recipes')
