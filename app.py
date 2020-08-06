b035d58c3974a5f43a3e6209f82a6e49db52daae
Matt Boland <mattjamesboland@gmail.com>
2020-08-06 15:01:36 +0000

    added register.html with a simple card form from materialize to test

--- app.py
+++ app.py
@@ -23,6 +23,11 @@ def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
