02c5be44057b7af3e456cf8e4b9de3c308fbb9fc
Matt Boland <mattjamesboland@gmail.com>
2020-08-07 11:07:51 +0000

    added a redirect session user to profile app.route in app.py

--- app.py
+++ app.py
@@ -79,7 +79,11 @@ def profile(username):
     # grab the session user's username from mongodb
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
