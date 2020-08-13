4508c5e78edb155644d12ed02a004590f19ec880
Matt Boland <mattjamesboland@gmail.com>
2020-08-13 17:22:09 +0000

    added delete_recipe app.route to app.py

--- app.py
+++ app.py
@@ -143,6 +143,13 @@ def edit_recipe(recipe_id):
                            categories=categories)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipe.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Successfully Deleted!!")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
