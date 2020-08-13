f3ff1bcb465201bf04ae7206e1815502d7af3af3
Matt Boland <mattjamesboland@gmail.com>
2020-08-13 17:00:45 +0000

    added cancel button and formatted edit_recipe.html, added a session.user for loop to recipe.html so only the session user can edit tasks not any user, updated app.route for edit_recipe so edit_recipe.html shows the recipe and items to be edited correctly.

--- app.py
+++ app.py
@@ -120,6 +120,22 @@ def add_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "recipe_title": request.form.get("recipe_title"),
+            "recipe_description": request.form.get("recipe_description"),
+            "recipe_method": request.form.get("recipe_method"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_serves": request.form.get("recipe_serves"),
+            "recipe_cookingtime": request.form.get("recipe_cookingtime"),
+            "recipe_preptime": request.form.get("recipe_preptime"),
+            "recipe_cuisine": request.form.get("recipe_cuisine"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipe.update({"_id": ObjectId(recipe_id)}, submit)
+        flash("Recipe Sucessfully Updated!")
+
     recipe = mongo.db.recipe.find_one({"_id": ObjectId(recipe_id)})
 
     categories = mongo.db.categories.find().sort("category_name", 1)
