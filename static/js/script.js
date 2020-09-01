a8a91afa02ca99e39d8bf3d7990ca93ca239d56b
Matt Boland <mattjamesboland@gmail.com>
2020-09-01 19:08:41 +0000

    Added better functionality to add_recipe.html for adding ingred and method along with the js in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -42,4 +42,38 @@ $(document).ready(function(){
 
 });
 
+ var ingredientField = $(".ingredient").length;
+    $("#add_ingredient").on("click", function () {
+        $("select").formSelect("destroy");
+        $(".new-ingredient:first").clone().insertBefore("#add_ingredient").find("input[type='text'], select, textarea").val("");
+        $("select").formSelect();
+        ingredientField += 1;
+    });
+
+    $("#remove_ingredient").on("click", function () {
+        if (ingredientField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-ingredient:last").remove();
+            /* ensure original ingredient line never gets deleted */
+            ingredientField-= 1;
+        }
+    });
+
+    var methodField = $(".method").length;
+    /* add new cloned item */
+    $("#add_step").on("click", function () {
+        $(".new-step:first").clone().insertBefore("#add_step").find("input[type='text'], select, textarea").val("");
+        /* increase counter so original direction is never removed */
+       methodField += 1;
+    });
+    /* delete last cloned item */
+    $("#remove_step").on("click", function () {
+        if ( methodField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-step:last").remove();
+            /* ensure original direction line never gets deleted */
+            methodField-= 1;
+        }
+    });
+
         
