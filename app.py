3d101ef87837a8869c05458091021d942057ebc1
Matt Boland <mattjamesboland@gmail.com>
2020-09-03 19:19:25 +0000

    Added some changes to recipe.html to render the recipe better and pagination in app.py with Guidos help during a mentoring session

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
+import math
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
@@ -16,12 +17,84 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# Pagination and sorting
+PAGE_SIZE = 8
+KEY_PAGE_SIZE = 'page_size'
+KEY_PAGE_NUMBER = 'page_number'
+KEY_TOTAL = 'total'
+KEY_PAGE_COUNT = 'page_count'
+KEY_ENTITIES = 'items'
+KEY_NEXT = 'next_uri'
+KEY_PREV = 'prev_uri'
+KEY_WORD_FIND = 'word_find'
+KEY_ORDER_BY = 'order_by'
+KEY_ORDER = 'order'
+
+
+def get_paginated_list(entity, query={}, **params):
+    page_size = int(params.get(KEY_PAGE_SIZE, PAGE_SIZE))
+    page_number = int(params.get(KEY_PAGE_NUMBER, 1))
+    order_by = params.get(KEY_ORDER_BY, '_id')
+    order = params.get(KEY_ORDER, 'asc')
+    order = pymongo.ASCENDING if order == 'asc' else pymongo.DESCENDING
+    if page_number < 1:
+        page_number = 1
+    offset = (page_number - 1) * page_size
+    items = []
+    word_find = ''
+    if KEY_WORD_FIND in params:
+        word_find = params.get(KEY_WORD_FIND)
+        if len(word_find.split()) > 0:
+            entity.create_index([("$**", 'text')])
+            result = entity.find({'$text': {'$search': word_find}})
+            items = result.sort(order_by, order).skip(offset).limit(page_size)
+        else:
+            items = entity.find().sort(
+                order_by, order
+            ).skip(offset).limit(page_size)
+    else:
+        items = entity.find(query).sort(order_by, order).skip(
+            offset).limit(page_size)
+    total_items = items.count()
+    if page_size > total_items:
+        page_size = total_items
+    if page_number < 1:
+        page_number = 1
+    if page_size:
+        page_count = math.ceil(total_items / page_size)
+    else:
+        page_count = 0
+    if page_number > page_count:
+        page_number = page_count
+    next_uri = {
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_NUMBER: page_number + 1
+    } if page_number < page_count else None
+    prev_uri = {
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_NUMBER: page_number - 1
+    } if page_number > 1 else None
+
+    return {
+        KEY_TOTAL: total_items,
+        KEY_PAGE_SIZE: page_size,
+        KEY_PAGE_COUNT: page_count,
+        KEY_PAGE_NUMBER: page_number,
+        KEY_NEXT: next_uri,
+        KEY_PREV: prev_uri,
+        KEY_WORD_FIND: word_find,
+        KEY_ORDER_BY: order_by,
+        KEY_ORDER: order,
+        KEY_ENTITIES: items
+        }
+
 
 @app.route('/')
-@app.route('/get_recipes')
+@app.route('/get_recipes', methods=['GET'])
 def get_recipes():
+    paginated_recipes = get_paginated_list(mongo.db.recipes, **request.args.to_dict())
     return render_template(
-        "recipes.html", recipes=list(mongo.db.recipe.find()))
+        "recipes.html", recipes=paginated_recipes)
 
 
 @app.route('/search', methods=['GET', 'POST'])
